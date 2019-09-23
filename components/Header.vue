614d4e7a6c1b494017a4764a3cfce526cef21a44
jorgechato <hack@jorgechato.com>
2019-09-23 21:49:49 +0200

    ADD header

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-  <header>
+  <header :class="">
     <ul>
       <li>
-        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/" class="banner">whereisjorge</nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/projects">Projects</nuxt-link>
+        <a href="//jorgechato.com">ARTICLES</a>
       </li>
     </ul>
   </header>
@@ -16,10 +16,47 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scope>
+$nav-height: 60px;
+
 header {
+  color: white;
   width: 100%;
-  background: red;
-  height: 60px;
+  height: $nav-height;
+  position: sticky;
+  z-index: 1;
+  border-bottom: 1px solid hsla(0,0%,100%,.1);
+  top: 0;
+  padding: 0 10%;
+
+  .banner {
+    font-weight: 700;
+    font-size: 28px;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+
+    li {
+      float: left;
+      line-height: $nav-height;
+
+      &:nth-child(even) {
+        margin-left: 2em;
+      }
+    }
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+
+    &:hover{
+      color: white;
+    }
+  }
 }
 </style>
