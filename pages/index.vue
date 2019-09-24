b2acf74224bd62130ff3a9ff51d1f057b097289f
jorgechato <hack@jorgechato.com>
2019-09-24 19:33:18 +0200

    ADD api integration

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,56 @@
   <div class="container">
     <div class="title">
       <div class="content">
-        <h1>Jorge is currently</h1>
-        <h2>in ____</h2>
-        <p>Leaving for Columbus in 21 days</p>
+        <h1>{{ name }} is currently</h1>
+        <h2>in {{ location.city }}</h2>
+        <p>
+          Leaving for {{ next_locations[0].country }} {{ daysUntilTrip }}
+        </p>
       </div>
     </div>
-    <my-map/>
+    <my-map
+      :origin="origin"
+      :location="location"/>
   </div>
 </template>
 
 <script>
 import Map from '~/components/Map.vue'
+import axios from 'axios'
+import moment from 'moment'
 
 
 export default {
   layout: 'main',
   components: {
     'my-map': Map
+  },
+  data() {
+    return {
+      origin: {
+        city: "Amsterdam",
+        country: "Netherlands",
+        lat: 52.1326,
+        lon: 5.2913,
+        timezone_id: ""
+      }
+    }
+  },
+  asyncData(context) {
+    return axios.get('http://localhost:5000/v1/location')
+      .then(res => {
+        let checkIn = moment.unix(res.data.next_locations[0].check_in)
+
+        return {
+          name: res.data.name,
+          city: res.data.city,
+          next_locations: res.data.next_locations,
+          location: res.data.location,
+          daysUntilTrip: moment(checkIn).fromNow()
+
+        }
+      })
+      .catch(e => context.error(e));
   }
 }
 </script>
